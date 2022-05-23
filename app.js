1d66e3f1657e456f9f24c647ace6509689fa2885
tujumg <tujumg@gmail.com>
2022-05-23 09:12:54 +0800

    add search function

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const mongoose = require('mongoose') // 載入 mongoose
 const db = mongoose.connection
+const bodyParser = require('body-parser')
+const Restaurant = require('./models/Restaurant')
 mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-const app = express()
-const port = 3000
 
 // 連線異常
 db.on('error', () => {
@@ -15,32 +17,48 @@ db.once('open', () => {
     console.log('mongodb connected!')
 })
 const port = 3000
-const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
+//餐廳首頁
 app.get('/',(req,res)=>{
-    
-    res.render('index',{restaurants : restaurantList.results})
+    Restaurant.find() // 取出 Restaurant model 裡的所有資料
+        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+        .then(restaurantData => res.render('index',{restaurantData}))  // 將資料傳給 index 樣板
+        .catch(error=>console.error(error)) // 錯誤處理
 })
-app.get('/restaurants/:restaurant_id',(req,res)=>{
-    const restaurant = restaurantList.results.find(restaurant => 
-        restaurant.id.toString() === req.params.restaurant_id)
-    res.render('show',{restaurant:restaurant})
+//選定特定餐廳
+app.get('/restaurants/:id',(req,res)=>{
+    const id = req.params.id
+    Restaurant.findById(id)
+        .lean()
+        .then(restaurantData => res.render('show',{restaurantData}))
+        .catch(err => console.log(err))
 })
+//搜尋餐廳
 app.get('/search',(req,res)=>{
-    console.log('req.query',req.query.keyword)
-    const keyword = req.query.keyword.toLocaleLowerCase()
-    const restaurants = restaurantList.results.filter(restaurant => {
-        return restaurant.name.toLowerCase().includes(keyword) ||
-        restaurant.category.toLowerCase().includes(keyword)
-    })
-    res.render('index',{restaurants : restaurants,keyword:keyword})
-})
+    
+    const keyword = req.query.keyword.trim().toLowerCase()
 
+    console.log('keyword',keyword)
+
+    Restaurant.find()
+        .lean()
+        .then((restaurantData) => {
+            const filterRestaurantsData = restaurantData.filter(
+                data =>
+                    data.name.toLowerCase().includes(keyword) ||
+          data.category.includes(keyword)
+            )
+            res.render('index', { restaurantData: filterRestaurantsData, keyword })
+        })
+        .catch(err => console.log(err))
+})
+    
 app.listen(port, ()=>{
     console.log(`Express is listening on localhost:${port}`)
 })
--- models/seeds/RestaurantSeeder.js
+++ models/seeds/RestaurantSeeder.js
@@ -1,20 +1,18 @@
 const mongoose = require('mongoose') // 載入 mongoose
-const todo = require('../todo')
+const Restaurant = require('../Restaurant')
 const db = mongoose.connection
-const restaurantList = require('./restaurant.json')
+const restaurantList = require('../../restaurant.json').results
 
 mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-const app = express()
 
 // 連線異常
 db.on('error', () => {
     console.log('mongodb error!')
-    for(let i = 1; i < 9;i++){
-        todo.create(restaurantList.results)
-    }
-    console.log('done')
 })
 // 連線成功
 db.once('open', () => {
     console.log('mongodb connected!')
+    Restaurant.create(restaurantList)
+            
+    console.log('done!')
 })
