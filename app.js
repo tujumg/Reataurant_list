f06c3fdeec7efbce3b7c5e9c735d6bda169f131f
tujumg <tujumg@gmail.com>
2022-06-19 16:31:22 +0800

    add user modules

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require('express')
-const app = express()
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
-const routes = require('./routes') // 引用路由器
 require('./config/mongoose')
+const session = require('express-session')
+const routes = require('./routes') // 引用路由器
+const app = express()
+const usePassport = require('./config/passport')
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(session({
+    secret:'ThisIsMySecret',
+    resave:false ,
+    saveUninitialized:true
+}))
 app.use(express.static('public'))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
+
 app.use(routes)  // 將 request 導入路由器
 
 app.listen(port, ()=>{
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express') // 引用 Express 與 Express 路由器
 const router = express.Router() // 準備引入路由模組
 const home = require('./modules/home') // 引入 home 模組程式碼
 const restaurant = require('./modules/restaurant')  // 引入 restaurant 模組程式碼
+const users = require('./modules/users')
  
 router.use('/', home)  // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/restaurants', restaurant)  // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurant 模組
+router.use('/users',users)
 
 module.exports = router // 匯出路由器
