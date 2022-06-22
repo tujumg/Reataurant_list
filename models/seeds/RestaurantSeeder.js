8cc440620b97556fe8a48f9b16d50ebc12dafcdd
tujumg <tujumg@gmail.com>
2022-06-23 01:13:49 +0800

    add seeder.js

--- models/seeds/RestaurantSeeder.js
+++ models/seeds/RestaurantSeeder.js
@@ -1,14 +1,50 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 const Restaurant = require('../Restaurant')
+const User = require('../user')
 const db = require('../../config/mongoose')
 const restaurantList = require('../../restaurant.json').results
-
+const SEED_USERS = [
+    {
+        name: 'user1',
+        email: 'user1@example.com',
+        password: '12345678',
+        restaurantId:[1,2,3]
+    },
+    {
+        name: 'user2',
+        email: 'user2@example.com',
+        password: '12345678',
+        restaurantId:[4,5,6]
+    }
+]
 // 連線成功
 db.once('open', () => {
-    console.log('mongodb connected!')
-    Restaurant.create(restaurantList)
-        .then(() => {
-            console.log('MongoDB restaurantSeeder created!')
+    return Promise.all(Array.from(SEED_USERS, SEED_USER => {
+        return bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(SEED_USER.password, salt))
+            .then(hash => {
+                return User.create({
+                    name: SEED_USER.name,
+                    email: SEED_USER.email,
+                    password: hash
+                }).then((user) => {
+                    const addRestaurants = restaurantList.filter(restaurant => {
+                        return SEED_USER.restaurantId.includes(restaurant.id)
+                    })
+                    return Promise.all(Array.from(addRestaurants, addRestaurant => {
+                        addRestaurant.userId = user._id
+                        return Restaurant.create(addRestaurant)
+                    }))
+                })
+            })
+    }))
+        .then(()=>{
+            console.log('Seeds done')
+            process.exit()
         })
-        .catch(error => console.log(error))
-        .finally(() => db.close())
+        .catch(err => console.log('err'))   
 })
